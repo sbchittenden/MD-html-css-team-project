68938a5cf7a0dbcb55e44883393a52084a1a315e
Denine Guy <denineguy@gmail.com>
2017-01-02 21:47:11 -0500

    Added the objects and properties for the top portion of the application

--- js/appData.js
+++ js/appData.js
@@ -1,6 +1,49 @@
 var collabAppData = {
+  roomName: {
+    title: 'Room Name',
+    description: 'Change the details of this chatroom here'
+  },
 
+  roles: {
+    description: 'Eu nulla morbi, eros placerat mattis lectus',
+    team: [
+      {
+        name: 'Alex Miller',
+        roleTitle: 'Product Manager'
+      },
+      {
+        name: 'James Morrison',
+        roleTitle: '3D Animator'
+      },
+      {
+        name: 'Miranda Lopez',
+        roleTitle: 'Concept Artist'
+      },
+      {
+        name: 'Alex Miller',
+        roleTitle: 'Product Manager'
+      }
+    ]
+  },
 
-
-
+  colors: {
+    description: 'These colors are for the entire room',
+    schemes: [
+      {
+        component: 'Application',
+        detail: 'The general color used for the application',
+        colorCode: '#DD77CB'
+      },
+      {
+        component: 'Box Value',
+        detail: 'The value of message backgrounds',
+        colorCode: '#6C84DC'
+      },
+      {
+        component: 'Text Color',
+        detail: 'The message text color',
+        colorCode: '#8D8D8D'
+      }
+    ]
+  }
 };
