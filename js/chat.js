22b39a3366e70d52f9f4fca1c3959098b418b46f
Sam Chittenden <sbchittenden@gmail.com>
2017-01-04 11:38:28 -0700

    Updated chat.js
    - new message object now gets pushed to collabAppData object
    - added function to append message to chat window

--- js/chat.js
+++ js/chat.js
@@ -1,28 +1,48 @@
 var chatInput = document.getElementById('chat-input');
 var submitBtn = document.querySelector('button.button.message-send');
 var chatUser = document.querySelector('div.message-left.input-left > .avatar');
+var coversationPanel = document.querySelector('.conversation-wrapper');
 
+// send message button listener
 submitBtn.addEventListener('click', sendMessage);
 
+
 function sendMessage() {
-  console.log('message submited!');
-  var messageBody = chatInput.value;
-  var timeStamp = new Date();
-  var userPhoto = chatUser.childNodes[1].src;
-  var userName = chatUser.id;
-  var messageObj;
+    console.log('message submited!');
+    var messageBody = chatInput.value;
+    var timeStamp = new Date();
+    var userPhoto = chatUser.childNodes[1].src;
+    var userName = chatUser.id;
+    var messageObj;
 
-  userPhoto = userPhoto.substr((userPhoto.lastIndexOf('/') + 1));
+    userPhoto = userPhoto.substr((userPhoto.lastIndexOf('/') + 1));
 
-  messageObj = {
-    userName: userName,
-    userPhoto: userPhoto,
-    timeStamp: timeStamp,
-    body: messageBody
-  };
+    // create the messageObj
+    messageObj = {
+        id: collabAppData.chatPanel.messageList.length + 1,
+        userName: userName,
+        userPhoto: userPhoto,
+        timeStamp: timeStamp,
+        body: messageBody
+    };
 
-  console.log(messageObj);
+    // add the new message object to the messageList array in appData object
+    collabAppData.chatPanel.messageList.push(messageObj);
 
-  return messageObj;
+    // log messageObj to the console
+    console.log(messageObj);
+   
+   // append new message to the chat window
+    appendMessage(messageObj);
 
+    return messageObj;
+
+}
+
+// function to append the new message to the chat window
+function appendMessage(m) {
+  var conversation = document.querySelector('.conversation-wrapper');
+  var messageWrap = document.querySelector('.message-right').cloneNode(true);
+  messageWrap.querySelector('.right-bubble').innerHTML = '<span class="time-stamp">'+ m.timeStamp.toTimeString().substr(0,5) +'</span><h1>James Morrison</h1><p>'+ m.body +'</p>';
+  conversation.appendChild(messageWrap);
 }
